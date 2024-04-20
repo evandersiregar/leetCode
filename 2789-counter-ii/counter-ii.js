1683ca68255e050e0236827338ffe893f60e6957
Evander Siregar <52197581+evandersiregar@users.noreply.github.com>
2024-04-20 08:30:08 +0700

    Time: 55 ms (76.99%) | Memory: 52 MB (15.73%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter = init;
+    return{
+        increment: () => counter+=1, decrement: () => counter-=1, reset: () => (counter=init),
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
